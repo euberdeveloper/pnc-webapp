94485c3734abd3f2de1dd22044b51a62d1a025b5
euberdeveloper <euberdeveloper@gmail.com>
2022-02-27 23:09:55 +0100

    feat(main.ts): start main
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,35 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import router from '@/router'
+import store/*, { ActionTypes } */from '@/store'
+// import VueGtag from "vue-gtag";
+import vuetify from '@/plugins/vuetify';
+import '@/plugins/cropper';
+// import api from '@/plugins/api';
+import '@/plugins/stored';
+import '@/plugins/validator';
 
-Vue.config.productionTip = false
+// import CONFIG from '@/config';
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+async function main() {
+  // if (CONFIG.GOOGLE_ANALYTICS_ID) {
+  //   Vue.use(VueGtag, {
+  //     config: { id: CONFIG.GOOGLE_ANALYTICS_ID }
+  //   }, router);
+  // }
+
+  // await store.dispatch(ActionTypes.STARTUP);
+  // api.errorHandler = (error) => {
+  //   store.dispatch(ActionTypes.HANDLE_API_ERROR, error);
+  // };
+
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+  }).$mount('#app');
+}
+main();
+
