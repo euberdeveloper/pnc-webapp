1f23f6277ec0743d19d5f8cb37856d87b1eb62df
euberdeveloper <euberdeveloper@gmail.com>
2022-02-27 23:03:10 +0100

    feat(config): add config file
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

---
+++ src/config/index.ts
@@ -0,0 +1,7 @@
+export default {
+    API_URL: process.env.VUE_APP_API_URL,
+    AXIOS_WITH_CREDENTIALS: process.env.VUE_APP_AXIOS_WITH_CREDENTIALS === 'true',
+    AXIOS_TIMEOUT: process.env.VUE_APP_AXIOS_TIMEOUT !== undefined ? +process.env.VUE_APP_AXIOS_TIMEOUT : 10000,
+    GOOGLE_ANALYTICS_ID: process.env.VUE_APP_GOOGLE_ANALYTICS_ID,
+    VERSION: process.env.PACKAGE_VERSION ?? 'sconosciuta'
+};
