742221c37724a75bee60cbda85af3a5ca362e64e
euberdeveloper <euberdeveloper@gmail.com>
2022-02-27 23:40:19 +0100

    feat(main.ts): add g analytics
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from '@/App.vue'
 import './registerServiceWorker'
 import router from '@/router'
 import store/*, { ActionTypes } */from '@/store'
-// import VueGtag from "vue-gtag";
+import VueGtag from "vue-gtag";
 import vuetify from '@/plugins/vuetify';
 import api from '@/plugins/api';
 import '@/plugins/validator';
@@ -11,11 +11,11 @@ import '@/plugins/validator';
 import CONFIG from '@/config';
 
 async function main() {
-  // if (CONFIG.GOOGLE_ANALYTICS_ID) {
-  //   Vue.use(VueGtag, {
-  //     config: { id: CONFIG.GOOGLE_ANALYTICS_ID }
-  //   }, router);
-  // }
+  if (CONFIG.GOOGLE_ANALYTICS_ID) {
+    Vue.use(VueGtag, {
+      config: { id: CONFIG.GOOGLE_ANALYTICS_ID }
+    }, router);
+  }
 
   // await store.dispatch(ActionTypes.STARTUP);
   // api.errorHandler = (error) => {
--- src/shims-plugins.d.ts
+++ src/shims-plugins.d.ts
@@ -1,6 +1,5 @@
 import { PncSDK } from 'pnc-sdk';
-// import { Validator } from '@/plugins/validator';
-
+import { Validator } from '@/plugins/validator';
 
 declare module 'vue/types/vue' {
     import Vue from 'vue';
