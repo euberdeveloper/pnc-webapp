f1b6728f3872db699043a4fac73380db11fcddaa
euberdeveloper <euberdeveloper@gmail.com>
2022-03-02 00:15:28 +0100

    refactor(config): translate unknown version
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,5 @@ export default {
     AXIOS_TIMEOUT: process.env.VUE_APP_AXIOS_TIMEOUT !== undefined ? +process.env.VUE_APP_AXIOS_TIMEOUT : 10000,
     STORAGE_PREFIX: process.env.VUE_APP_STORAGE_PREFIX,
     GOOGLE_ANALYTICS_ID: process.env.VUE_APP_GOOGLE_ANALYTICS_ID,
-    VERSION: process.env.PACKAGE_VERSION ?? 'sconosciuta'
+    VERSION: process.env.PACKAGE_VERSION ?? 'Unknown'
 };
